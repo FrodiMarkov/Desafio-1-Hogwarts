8e5c8f41fdde01e86d9f00dc3e56cc8bed99a979
FrodiMarkov <swdarkfrodiangel@gmail.com>
2025-11-16 03:25:06 +0100

    Cambios en la activity de login de Dumbledore.

---
+++ Desafio1/app/src/main/java/com/example/desafio1/AdminFragment.kt
@@ -0,0 +1,30 @@
+package com.example.desafio1
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AdminFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AdminFragment()
+    }
+
+    private val viewModel: AdminViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_admin, container, false)
+    }
+}
--- Desafio1/app/src/main/java/com/example/desafio1/MainActivity.kt
+++ Desafio1/app/src/main/java/com/example/desafio1/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.example.desafio1
 
-import Api.EncuestaNetwork.retrofit
+import Api.HowartsNetwork.retrofit
+import ViewModel.LoginViewModel
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 //import necesario para glide
 import com.bumptech.glide.Glide
-import com.example.desafio1.API.usuariosAPI
 import com.example.desafio1.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 import kotlin.jvm.java
@@ -19,6 +20,7 @@ import kotlin.jvm.java
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,50 +36,38 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        binding.btLogin.setOnClickListener {
-            val emailLogin = binding.etUser.text.toString()
-            val passwordLogin = binding.etPassw.text.toString()
-
-            lifecycleScope.launch {
-                try {
-                    val usuarios = retrofit.listarUsuariosConRoles()
-                    val usuario = usuarios.find { it.nombre == emailLogin && it.contrasena == passwordLogin }
-
-                    if (usuario != null) {
-                        when {
-                            usuario.roles.contains(1) -> {
-                                val intent = Intent(this@MainActivity, AlumnoActivity::class.java)
-                                startActivity(intent)
-                                finish()
-                            }
-                            usuario.roles.contains(4) -> {
-                                val intent = Intent(this@MainActivity, DumbledorActivity::class.java)
-                                intent.putExtra("usuario_id", usuario.id)
-                                startActivity(intent)
-                                finish()
-                            }
-                            usuario.roles.contains(2) -> {
-                                val intent = Intent(this@MainActivity, ProfesorActivity::class.java)
-                                intent.putExtra("usuario_id", usuario.id)
-                                startActivity(intent)
-                                finish()
-                            }
-                            else -> Toast.makeText(this@MainActivity, "Rol no reconocido", Toast.LENGTH_SHORT).show()
-                        }
-                    } else {
-                        Toast.makeText(this@MainActivity, "Email o contraseña incorrectos", Toast.LENGTH_SHORT).show()
-                    }
-
-                } catch (e: Exception) {
-                    Toast.makeText(this@MainActivity, "Error en la conexión: ${e.message}", Toast.LENGTH_SHORT).show()
+        viewModel.usuarioLogueado.observe(this) { usuario ->
+            usuario?.let {
+                when {
+                    it.roles.contains(1) -> startActivity(Intent(this, AlumnoActivity::class.java))
+                    it.roles.contains(4) -> startActivity(Intent(this, DumbledorActivity::class.java).apply {
+                        putExtra("usuario_id", it.id)
+                    })
+                    it.roles.contains(2) -> startActivity(Intent(this, ProfesorActivity::class.java).apply {
+                        putExtra("usuario_id", it.id)
+                    })
+                    else -> Toast.makeText(this, "Rol no reconocido", Toast.LENGTH_SHORT).show()
                 }
+                finish()
             }
+        }
+
+        viewModel.error.observe(this) { mensaje ->
+            mensaje?.let {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.btLogin.setOnClickListener {
+            val usuarioLogin = binding.etUser.text.toString()
+            val passwordLogin = binding.etPassw.text.toString()
+            viewModel.login(usuarioLogin, passwordLogin)
         }
 
         Glide.with(this)
             //que poner para que sea un gif y no una imagen
             .asGif()
-            .load(R.drawable.background2) // reemplaza con tu archivo GIF
+            .load(R.drawable.background2)
             .into(binding.IVBackground)
     }
 }
